0d4f33834936148ee6ecbef658898b1c592e8fec
Sourav163 <souravroutray2016@gmail.com>
2024-01-06 22:43:18 +0530

    Lambda Expression (Functional Interface)

---
+++ LambdaExpression.java
@@ -0,0 +1,15 @@
+package Advance_Java;
+
+@FunctionalInterface
+interface FunctionalInterface0 {
+    public void meth();
+}
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        FunctionalInterface0 obj = () -> {
+            System.out.println("I am meth()");
+        };
+        obj.meth();
+    }
+}
